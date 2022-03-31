d506360b9a422a4edecdefaa4158857eb4b75474
kavunap <kavunap>
2022-03-31 21:19:33 +0200

    database con

--- static_app/settings.py
+++ static_app/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['static-app-sample.herokuapp.com']
 
 
 # Application definition
