b7ece44b0209c2c8564578005fc6f5161871282c
kavunap <kavunap>
2022-04-02 23:17:55 +0200

    my commit

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+# from author.models import Author
 
 class Blog(models.Model):
+    # author = models.ForeignKey(
+    #     'Author',
+    #     on_delete=models.CASCADE,
+    # )
     title = models.CharField(max_length=255)
     content = models.TextField()
+
+# class User
+
